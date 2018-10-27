e797179483417010c3a1dee90f29e755ffb8e073
mosu forge <mosu.forge@protonmail.com>
2018-10-26 17:23:14 -0700

    save window position and size between loads

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -2,6 +2,7 @@ import { app, ipcMain, BrowserWindow, Menu, dialog } from "electron"
 import { Backend } from "./modules/backend"
 import menuTemplate from "./menu"
 const portscanner = require("portscanner")
+const windowStateKeeper = require("electron-window-state")
 
 /**
  * Set `__statics` path to static files in production;
@@ -38,13 +39,20 @@ function createWindow() {
     /**
      * Initial window options
      */
+
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 800,
+        defaultHeight: 600
+    })
+
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
         minWidth: 800,
         minHeight: 600,
-        icon: require("path").join(__statics, "icon_512x512.png"),
-        useContentSize: true
+        icon: require("path").join(__statics, "icon_512x512.png")
     })
 
     mainWindow.on("close", (e) => {
@@ -121,7 +129,7 @@ function createWindow() {
     })
 
     mainWindow.loadURL(process.env.APP_URL)
-
+    mainWindowState.manage(mainWindow)
 }
 
 app.on("ready", () => {
