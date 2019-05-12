3c577d839814e0674c5f547495d3b8cda9f4ce83
mosu forge <mosu.forge@protonmail.com>
2019-05-12 10:37:04 -0700

    seed word validation

--- src-electron/main-process/modules/wallet-rpc.js
+++ src-electron/main-process/modules/wallet-rpc.js
@@ -261,7 +261,7 @@ export class WalletRPC {
             this.wallet_state.name = filename
             this.wallet_state.open = true
 
-            this.finalizeNewWallet(filename)
+            this.finalizeNewWallet(filename, true)
 
         })
 
@@ -291,6 +291,21 @@ export class WalletRPC {
             refresh_start_height = 0
         }
         seed = seed.trim().replace(/\s{2,}/g, " ")
+
+        let seed_words = seed.split(" ")
+        switch(seed_words.length) {
+            case 14:
+            case 24:
+            case 25:
+                break
+            case 26:
+                seed_words.pop()
+                seed = seed_words.join(" ")
+                break
+            default:
+                this.sendGateway("set_wallet_error", {status: {code: -1, message:"Invalid seed word length"}})
+                return
+        }
 
         this.sendRPC("restore_wallet", {
             filename,
@@ -424,7 +439,7 @@ export class WalletRPC {
 
     }
 
-    finalizeNewWallet(filename) {
+    finalizeNewWallet(filename, newly_created=false) {
 
         Promise.all([
             this.sendRPC("get_address"),
@@ -441,7 +456,8 @@ export class WalletRPC {
                     balance: 0,
                     unlocked_balance: 0,
                     height: 0,
-                    view_only: false
+                    view_only: false,
+                    newly_created
                 },
                 secret: {
                     mnemonic: "",
--- src/pages/wallet-select/created.vue
+++ src/pages/wallet-select/created.vue
@@ -4,13 +4,22 @@
     <AddressHeader :address="info.address" :title="info.name" />
 
     <template v-if="secret.mnemonic">
+        <template v-if="info.newly_created">
+            <div class="row q-py-sm q-px-md q-mt-lg bg-amber-3 text-dark round-borders">
+                Backup your seed words in a secure location! This is the only way to access your funds if you switch devices.
+            </div>
+        </template>
         <h6 class="q-mb-xs q-mt-lg">Seed words</h6>
         <div class="row">
-            <div class="col">
+            <div class="col" v-if="mnemonic_words_confirm.length">
+                Hidden while you confirm you mnemonic seed words. Make sure to write these down!
+            </div>
+            <div class="col" v-else>
                 {{ secret.mnemonic }}
             </div>
             <div class="q-item-side">
                 <q-btn
+                    v-bind:class="{ invisible: mnemonic_words_confirm.length }"
                     color="primary" style="width:25px;"
                     size="sm" icon="file_copy"
                     @click="copyPrivateKey('mnemonic', $event)">
@@ -60,8 +69,47 @@
         </div>
     </template>
 
+
+
+    <div v-if="info.newly_created">
+        <h6 class="q-mb-md">Confirm your mnemonic seed words</h6>
+        <p>Enter the first seven words of your mnemonic seed in the correct order:</p>
+
+        <div v-bind:class="{ row: true, 'q-pa-sm': true, 'round-borders': true, 'bg-grey-4':theme=='light', 'bg-dark':theme=='dark' }" style="min-height:48px">
+            <template v-for="(word, index) in mnemonic_words_confirm">
+                <div class="col-auto q-px-xs">
+                    <q-chip class="non-selectable" color="primary" closable @hide="removeWord(index)">{{ word }}</q-chip>
+                </div>
+            </template>
+            <template v-if="mnemonic_words_confirm.length">
+                <q-btn icon="backspace" dense flat @click="removeWord(mnemonic_words_confirm.length-1)" />
+            </template>
+        </div>
+
+        <div class="row gutter-xs q-mt-md" v-if="mnemonic_words_shuffle.length">
+            <template v-for="(word, index) in mnemonic_words_shuffle">
+                <div class="col-auto">
+                    <q-btn
+                        no-caps
+                        :label="word"
+                        :color="mnemonic_words_confirm_i.includes(index) ? (theme=='dark'?'dark':'standard') : 'primary'"
+                        :text-color="mnemonic_words_confirm_i.includes(index) ? (theme=='dark'?'white':'dark') : 'white'"
+                        :disable="mnemonic_words_confirm_i.includes(index)"
+                        @click="addWord(index)" />
+                </div>
+            </template>
+        </div>
+    </div>
+
+
     <q-field>
-        <q-btn class="q-mt-lg" color="primary" @click="open" label="Open wallet" />
+        <q-btn
+            label="Open wallet"
+            class="q-mt-lg"
+            :color="!can_continue ? (theme=='dark'?'dark':'standard') : 'primary'"
+            :text-color="!can_continue ? (theme=='dark'?'white':'dark') : 'white'"
+            :disabled="!can_continue"
+            @click="open" />
     </q-field>
 
 </q-page>
@@ -72,11 +120,48 @@ const { clipboard } = require("electron")
 import { mapState } from "vuex"
 import AddressHeader from "components/address_header"
 export default {
-    computed: mapState({
-        info: state => state.gateway.wallet.info,
-        secret: state => state.gateway.wallet.secret,
-    }),
+    computed: {
+        ...mapState({
+            theme: state => state.gateway.app.config.appearance.theme,
+            info: state => state.gateway.wallet.info,
+            secret: state => state.gateway.wallet.secret,
+        }),
+        can_continue() {
+            return !this.info.newly_created || this.mnemonic_words_confirm.join(" ") == this.secret.mnemonic.split(" ").slice(0,7).join(" ")
+        },
+        mnemonic_words_confirm() {
+            let mnemonic_words_confirm = []
+            for(let i of this.mnemonic_words_confirm_i) {
+                mnemonic_words_confirm.push(this.mnemonic_words_shuffle[i])
+            }
+            return mnemonic_words_confirm
+        }
+    },
+    data() {
+        return {
+            mnemonic_words_confirm_i: [],
+            mnemonic_words_shuffle: []
+        }
+    },
+    mounted() {
+        if(this.secret.mnemonic) {
+            let words = this.secret.mnemonic.split(" ").slice(0,7)
+            for(let i = words.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1))
+                const words_i = words[i]
+                words[i] = words[j]
+                words[j] = words_i
+            }
+            this.mnemonic_words_shuffle = words
+        }
+    },
     methods: {
+        addWord(index) {
+            this.mnemonic_words_confirm_i.push(index)
+        },
+        removeWord(index) {
+            this.mnemonic_words_confirm_i.splice(index, 1)
+        },
         open() {
             setTimeout(() => {
                 this.$store.commit("gateway/set_wallet_data", {
